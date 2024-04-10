a344aa03e20082c55b35329b4fa76a624572a845
khushi <sourabhpatel63679@gmail.com>
2024-04-10 11:31:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App=()=>{
   const ans=useSelector((state)=>state.todo.task);
   const dispatch=useDispatch("");
   const [input, setInput]=useState("");
+
   let sn=0;
   const workAdd=()=>{
     dispatch(addTask(input))
@@ -16,6 +17,17 @@ const App=()=>{
       <tr>
         <td>[{sn}].</td>
         <td>{key.work}</td>
+        <td>
+          <button onClick={()=>{mydel}}>Delete</button>
+        </td>
+        <td>
+          <button>complete</button>
+        </td>
+        <td> 
+          <button>reopen
+            
+          </button>
+        </td>
       </tr>
     )
   });
@@ -26,6 +38,15 @@ const App=()=>{
     Enter your task: <input type="taxt" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
     <button onClick={workAdd}> Add Task </button>
     <hr/>
+    <table>
+      <tr>
+        <td>slot</td>
+        <td>work additing</td>
+        <th></th>
+        <th></th>
+      </tr>
+    </table>
+
     {myans}
     </>
   );
--- src/TodoSlice.js
+++ src/TodoSlice.js
@@ -8,9 +8,26 @@ const todoSlice=createSlice({
     initialState:initialState,
     reducers:{
         addTask:(state,action)=>{
-            state.task.push({id:Date.now(), work:action.payload});
+            state.task.push({id:Date.now(), work:action.payload, status:true});
+        },
+        recDel:(state, action)=>{
+            state.task=state.task.filter(function(key){
+                if(key.id!=action.payload){
+                    return true
+                }
+                
+            })
+        // recDel:(state, action)=>{
+        //     state.task= state.task.filter(key=>key.id!=action.payload);
+        // }
+        reComplete:(state, action)=>{
+            for (var i=0; i<state.task.lenght; ++)
+            {
+                if(state.task[i].id==action.payload)
+            }
+        }
         }
     }
 });
-export const {addTask}=todoSlice.actions;
+export const {addTask, recDel,reComplete}=todoSlice.actions;
 export default todoSlice.reducer;
